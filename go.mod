4f40efd2a639d15fcb09f082c75fad5b1662e17b
Mazur <vlad882008@mail.ru>
2024-04-29 19:48:28 +0300

    plus c int

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Mazur1380/vlad1380
+module github.com/Mazur1380/vlad1380/v2
 
 go 1.22.0
--- math/math.go
+++ math/math.go
@@ -1,7 +1,7 @@
 package math
 
-func Sum(a int, b int) int {
-	return a + b
+func Sum(a int, b int, c int) int {
+	return a + b + c
 }
 
 func Mul(a int, b int) int {
